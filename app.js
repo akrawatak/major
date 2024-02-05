10118fc3257a09151af141f5595d7ebdf6408b95
Akash Rawat <akrawatak25@gmail.com>
2024-02-05 20:47:51 +0530

    update

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const path =require("path");
-
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 const MONGO_URL  = "mongodb://127.0.0.1:27017/wanderlust";
+
 
 main().then(()=> {
     console.log("connected to DB");
@@ -20,6 +22,9 @@ async function main() {
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
+app.engine('ejs',ejsMate);
+app.use(express.static(path.join(__dirname,"public")));
 
 app.get("/",(req,res)=>{
     res.send("Hi , I am root");
@@ -34,7 +39,7 @@ app.get("/listings", async(req, res) => {
 
 //New route
 app.get("/listings/new", (req,res)=> {
-    res.render("listing/new.ejs");
+    res.render("listings/new.ejs");
 });
 
 //show route
@@ -46,7 +51,36 @@ app.get("/listings/:id",async(req,res)=>{
 });
 
 
+//Create Route
+app.post("/listings",async (req,res)=> {
+     let listing = req.body.listing;
+    const  newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+});
 
+
+//Edit route
+app.get("/listings/:id/edit", async (req,res)=>{
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs",{listing});
+});
+
+//Update Route
+app.put("/listings/:id",async (req,res)=>{
+    let {id} = req.params;
+   await Listing.findByIdAndUpdate(id,{...req.body.listing});
+   res.redirect(`/listings/${id}`);
+});
+
+//Delete Route
+app.delete("/listings/:id", async(req,res)=>{
+    let {id} = req.params;
+  let deletedListing =   await Listing.findByIdAndDelete(id);
+  console.log(deletedListing);
+  res.redirect("/listings");
+})
 
 // app.get("/testListing",async (req,res)=>{
 //    let sampleListing = new Listing({
--- models/listing.js
+++ models/listing.js
@@ -8,13 +8,22 @@ const listingSchema = new Schema({
     },
     description: String,
     image: {
+        url:{
         type: String,
         default: 
         "https://www.google.com/url?sa=i&url=https%3A%2F%2Fpetapixel.com%2Fwhat-is-unsplash%2F&psig=AOvVaw0gcfBvytfecUDI2u_lNYJE&ust=1706734676618000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCKDo9aKAhoQDFQAAAAAdAAAAABAE",
         set: (v) => 
         v === "" 
         ? "https://www.google.com/url?sa=i&url=https%3A%2F%2Fpetapixel.com%2Fwhat-is-unsplash%2F&psig=AOvVaw0gcfBvytfecUDI2u_lNYJE&ust=1706734676618000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCKDo9aKAhoQDFQAAAAAdAAAAABAE"
-         : v,   
+         : v,
+         required:true,
+        },
+         
+        filename:{
+           type:String,
+           required:true,
+       },
+        
     },
     price: Number,
     location: String,
